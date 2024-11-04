e468a3ac24970bff0c4f6ffca1128ee98fd29e0d
Alexey Shmagin <alexsmgn@gmail.com>
2024-11-04 19:43:57 +0300

    Первая проверка 12 ФЗ

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
